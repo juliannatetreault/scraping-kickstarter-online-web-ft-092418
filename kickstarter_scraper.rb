c3790dc6c9dd61baaeac761d2502466b85aace64
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-11 09:51:15 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ def create_project_hash
   binding.pry
 end
 
+create_project_hash
+
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
-
-create_project_hash
