436f9cc23bb4028fa52a487568cb1581e904f64d
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-11 09:57:51 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,3 +17,5 @@ create_project_hash
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 #location : project.css(".location-name").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
